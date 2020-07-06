824f7a799539c1775ed5e5f84b61d057aa4bfeab
Agugu95 <fkrns1744@naver.com>
2020-07-06 11:27:40 +0900

    [solved: 원형 큐를 이용한 수열 반복]

--- src/main/java/programers/모의고사.java
+++ src/main/java/programers/모의고사.java
@@ -1,8 +1,6 @@
 package programers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class 모의고사 {
@@ -13,13 +11,13 @@ public class 모의고사 {
         int[] num2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
         int[] num3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == num1[i]) {
+            if (answers[i] == num1[i] % 5) {
                 temp[0]++;
             }
-            if (answers[i] == num2[i]) {
+            if (answers[i] == num2[i] % 8) {
                 temp[1]++;
             }
-            if (answers[i] == num3[i]) {
+            if (answers[i] == num3[i] % 10) {
                 temp[2]++;
             }
         }
