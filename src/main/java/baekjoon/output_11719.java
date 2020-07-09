dc24f4337ee3f5eeeaa8e7eee6c354e11dd3c157
Agugu95 <fkrns1744@naver.com>
2020-07-09 22:23:31 +0900

    [solved: 백준 11719 그대로 출력하기]

---
+++ src/main/java/baekjoon/output_11719.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class output_11719 {
+    public static void main(String args[]){
+        ArrayList<String> words = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String word = sc.nextLine();
+            if(word.length()>100 || !word.matches("[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\" \"]*")) {
+                break;
+            }
+            words.add(word);
+        }
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
