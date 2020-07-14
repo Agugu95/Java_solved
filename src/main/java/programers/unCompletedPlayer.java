01b1e58f21e25023c1109ae0f5abd4a177d3b41f
Agugu95 <fkrns1744@naver.com>
2020-07-14 23:34:26 +0900

    [solved: 프로그래머스 완주 하지 못한 선수 - 해시맵 개선]

--- src/main/java/programers/unCompletedPlayer.java
+++ src/main/java/programers/unCompletedPlayer.java
@@ -1,21 +1,33 @@
 package programers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class unCompletedPlayer {
     public String solution(String[] participant, String[] completion) {
-        StringBuilder answer = new StringBuilder();
-        for (String s : participant) {
-            boolean completed = true;
-            for (int j = 0; j < completion.length; j++) {
-                if (s.equals(completion[j])) {
-                    completion[j] = null;
-                    completed = false;
-                    break;
-                }
-            }
-            if (completed) {
-                answer.append(s);
+        String answer = "";
+        int count = 0;
+        Map<String, Integer> hashMap = new HashMap<>();
+
+        for (String name : participant) { // 참가자 체크, 해시맵 삽입
+            if (hashMap.get(name) == null) {
+                hashMap.put(name,1);
+            } else {
+                count = hashMap.get(name) + 1;
+                hashMap.put(name, count);
             }
         }
-        return answer.toString();
+
+        for (String complement : completion) { // 항상 1 작기 때문에 -1 후 get, 완주자 삽입
+            count = hashMap.get(complement) - 1;
+            hashMap.put(complement, count);
+        }
+
+        for (String name : hashMap.keySet()) { // 여기서 가져온 이름은 미 완주자
+            if (hashMap.get(name) == 1) {
+                answer = name;
+            }
+        }
+        return answer;
     }
 }
