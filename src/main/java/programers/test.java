4e13b342397b4c9835b29808831b33d8fbc841f1
rwh <fkrns1744@naver.com>
2021-03-08 17:22:12 +0900

    [test]

---
+++ src/main/java/programers/test.java
@@ -0,0 +1,12 @@
+package programers;
+
+public class test {
+    public static void main(String[] args) {
+        int i;
+        int sum = 0;
+        for(i = 1; i <= 10; i = i + 2){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
